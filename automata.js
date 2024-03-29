5df8503a945c822df35d5816a20ed26d198d1e8c
Octavio Duarte <octiduarte@hotmail.com>
2024-03-29 19:34:06 -0300

    agrgado de html

--- automata.js
+++ automata.js
@@ -8,9 +8,9 @@ let alfabeto = "ab";
 
 // Funcion de Transicion
 let funcionTransicion = {
-    q0: { [alfabeto[0]]: q0, [alfabeto[1]]: q1 },
-    q1: { [alfabeto[0]]: q2, [alfabeto[1]]: q0 },
-    q2: { [alfabeto[0]]: q2, [alfabeto[1]]: q1 }
+  q0: { [alfabeto[0]]: q0, [alfabeto[1]]: q1 },
+  q1: { [alfabeto[0]]: q2, [alfabeto[1]]: q0 },
+  q2: { [alfabeto[0]]: q2, [alfabeto[1]]: q1 },
 };
 
 // Estado Inicial
@@ -19,25 +19,34 @@ let estadoInicial = q0;
 // Estado de Aceptacion
 let estadoAceptacion = q2;
 
-const prompt = require("prompt-sync")();
-const palabraUsuario = prompt("Ingrese una palabra: ");
-
 chequearPalabraIngresada(palabraUsuario);
+
+
+
+function verificarPalabra() {
+  let palabraUsuario = document.getElementById("palabra").value;
+  chequearPalabraIngresada(palabraUsuario);
+}
 
 function chequearPalabraIngresada(palabra) {
     let palabraValida = true;
     for (let index = 0; index < palabra.length; index++) {
-        const element = palabra[index];
-        console.log(element)
-        if (!alfabeto.includes(element)) { 
-            palabraValida = false;
-            break;
-        }
+      const element = palabra[index];
+      console.log(element);
+      if (!alfabeto.includes(element)) {
+        palabraValida = false;
+        break;
+      }
     }
+  
+    mostrarResultado(palabraValida);
+  }
 
-    if (palabraValida) {
-        console.log("Palabra válida.");
-    } else {
-        console.log("Palabra no válida.");
-    }
+function mostrarResultado(resultado) {
+  let resultadoElemento = document.getElementById("resultado");
+  if (resultado) {
+    resultadoElemento.textContent = "palabra valida";
+  } else {
+    resultadoElemento.textContent = "palabra invalida";
+  }
 }
