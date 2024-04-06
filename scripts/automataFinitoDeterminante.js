2833c0e136c549d9e4e200711d329cefe4906c40
Octavio Duarte <octiduarte@hotmail.com>
2024-04-06 17:16:19 -0300

    interfaz actualizada

--- scripts/automataFinitoDeterminante.js
+++ scripts/automataFinitoDeterminante.js
@@ -2,22 +2,26 @@
 let q0 = "q0";
 let q1 = "q1";
 let q2 = "q2";
+let q3 = "q3";
+let q4 = "q4";
 
 // Alfabeto
 let alfabeto = "ab";
 
 // Funcion de Transicion
 let funcionTransicion = {
-  q0: { [alfabeto[0]]: q0, [alfabeto[1]]: q1 },
-  q1: { [alfabeto[0]]: q2, [alfabeto[1]]: q0 },
-  q2: { [alfabeto[0]]: q2, [alfabeto[1]]: q1 },
+  q0: { [alfabeto[0]]: q1, [alfabeto[1]]: q4 },
+  q1: { [alfabeto[0]]: q2, [alfabeto[1]]: q3 },
+  q2: { [alfabeto[0]]: q4, [alfabeto[1]]: q4 },
+  q3: { [alfabeto[0]]: q2, [alfabeto[1]]: q3 },
+  q4: { [alfabeto[0]]: q4, [alfabeto[1]]: q4 },
 };
 
 // Estado Inicial
 let estadoInicial = q0;
 
 // Estado de Aceptacion
-let estadoAceptacion = q2;
+let estadoAceptacion = [q2, q3];
 
 let estadoActual = null;
 
@@ -41,17 +45,35 @@ function chequearPalabraIngresada(palabra) {
 
 function mostrarResultado(resultado) {
   palabraUsuario = document.getElementById("palabra").value;
-  resultadoElemento = document.getElementById("resultado");
+  resultadoPositivo = document.getElementById("resultadoPositivo");
+  resultadoNegativo = document.getElementById("resultadoNegativo");
   if (resultado == false) {
     alert("Ingrese de nuevo la Palabra");
     document.getElementById("palabra").value = "";
     resultadoElemento.innerHTML = "";
   } else {
+    let palabraAceptada = false;
     cambiarEstado(palabraUsuario);
-    if (estadoActual == estadoAceptacion) {
-      resultadoElemento.innerHTML = "Palabra Aceptada";
+    for (let index = 0; index < estadoAceptacion.length; index++) {
+      const element = estadoAceptacion[index];
+      console.log("elemento " + element);
+      console.log(estadoAceptacion);
+      if (estadoActual === element) {
+        palabraAceptada = true;
+        break;
+      }
+    }
+    if (palabraAceptada) {
+      resultadoNegativo.style.setProperty('display', 'none', 'important') ;
+      resultadoPositivo.style.display = "block";
+      resultadoPositivo.innerHTML = "Palabra Aceptada";
+      
     } else {
-      resultadoElemento.innerHTML = "Palabra NO Aceptada";
+      resultadoPositivo.style.setProperty('display', 'none', 'important') ;
+      resultadoNegativo.style.display = "block";
+      resultadoNegativo.innerHTML = "Palabra No Aceptada";
+    
+      
     }
   }
 }
