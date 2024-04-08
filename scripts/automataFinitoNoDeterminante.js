2384c78b2cc28a4612d753a9dcc5ff4a10b7671d
Martin <trochezmartin@gmail.com>
2024-04-08 00:28:22 -0300

    Cambiando Algunos html para que se pueda navegar desde cualquiera de ellos a otros y agregando el script de automata finito no determinado

--- scripts/automataFinitoNoDeterminante.js
+++ scripts/automataFinitoNoDeterminante.js
@@ -1,73 +1,135 @@
-const PromptSync = require("prompt-sync");
-
 // Estados
-let q0 = "q0";
-let q1 = "q1";
-let q2 = "q2";
+const estados = ["q0", "q1", "q2"];
 
 // Alfabeto
-let alfabeto = "ab";
+const alfabeto = "ab";
 
 // Funcion de Transicion
-let funcionTransicion = {
-    q0: { [alfabeto[0]]: q0, [alfabeto[1]]: q1 },
-    q1: { [alfabeto[0]]: q2, [alfabeto[1]]: q0 },
-    q2: { [alfabeto[0]]: q2, [alfabeto[1]]: q1 }
+const funcionTransicion = {
+    [estados[0]]: [[alfabeto[0], estados[1]], [alfabeto[1], []]],
+    [estados[1]]: [[alfabeto[0], estados[2]], [alfabeto[1], estados[1]], [alfabeto[1], estados[2]]],
+    [estados[2]]: [[alfabeto[0], []], [alfabeto[1], []]],
 };
 
+
+
+
 // Estado Inicial
-let estadoInicial = q0;
+const estadoInicial = estados[0];
 
 // Estado de Aceptacion
-let estadoAceptacion = q2;
+const estadosAceptacion = [estados[2]];
 
 let estadoActual = null;
 
-const prompt = require("prompt-sync")();
-const palabraUsuario = prompt("Ingrese una palabra: ");
 
+let estadoActualImagen = document.getElementById("estadoActualImagen");
+
+function mostrarImagen() {
+    switch (estadoActual) {
+        case estados[0]:
+            estadoActualImagen.src = "./automatas/no_determinante_primer/q0.jpg";
+            break;
+        case estados[1]:
+            estadoActualImagen.src = "./automatas/no_determinante_primer/q1.jpg";
+            break;
+        case estados[2]:
+            estadoActualImagen.src = "./automatas/no_determinante_primer/q2.jpg";
+            break;
+        default:
+            estadoActualImagen.src = "./automatas/no_determinante_primer/sinPintar.jpg";
+            break;
+    }
+
+}
+
+function verificarPalabra() {
+    let palabraUsuario = document.getElementById("palabra").value;
+    chequearPalabraIngresada(palabraUsuario);
+}
 
 function chequearPalabraIngresada(palabra) {
     let palabraValida = true;
     for (let index = 0; index < palabra.length; index++) {
         const element = palabra[index];
+        console.log(element);
         if (!alfabeto.includes(element)) {
             palabraValida = false;
             break;
         }
     }
+    mostrarResultado(palabraValida);
+}
 
-    if (palabraValida) {
-        console.log("Palabra válida.");
+async function mostrarResultado(resultado) {
+    palabraUsuario = document.getElementById("palabra").value;
+    resultadoPositivo = document.getElementById("resultadoPositivo");
+    resultadoNegativo = document.getElementById("resultadoNegativo");
+    if (resultado == false) {
+        alert("Ingrese de nuevo la Palabra");
+        document.getElementById("palabra").value = "";
+        resultadoElemento.innerHTML = "";
     } else {
-        console.log("Palabra no válida.");
+        let palabraAceptada = false;
+        cambiarEstado(palabraUsuario);
+        if (estadosAceptacion.includes(estadoActual)) {
+            palabraAceptada = true;
+        }
+        if (palabraAceptada) {
+            resultadoNegativo.style.setProperty('display', 'none', 'important');
+            resultadoPositivo.style.display = "block";
+            resultadoPositivo.innerHTML = "Palabra Aceptada";
+        } else {
+            resultadoPositivo.style.setProperty('display', 'none', 'important');
+            resultadoNegativo.style.display = "block";
+            resultadoNegativo.innerHTML = "Palabra No Aceptada";
+        }
     }
-    return palabraValida
 }
 
-
-function cambiarEstado(palabra) {
-    if (estadoActual == null) {
-        estadoActual = estadoInicial;
+async function cambiarEstado(palabra, estadoTemp = null) {
+    let palabraTamanio = palabra.length
+    estadoActual = estadoTemp
+    if (estadoActual === null) {
+        estadoActual = estadoInicial
     }
-    for (let index = 0; index < palabra.length; index++) {
-        const element = palabra[index];
-        let estadoAnterior = estadoActual;
-        console.log(funcionTransicion[estadoAnterior])
-        let temp = funcionTransicion[estadoAnterior]
-        console.log(temp[element])
-        estadoActual = temp[element]
-    }
+    mostrarImagen();
+    console.log("estado actual " + estadoActual);
+    console.log("Palabra " + palabra);
+    console.log("Palabra[0] " + palabra[0]);
 
+    for (let index = 0; index < estadosAceptacion.length; index++) {
+        const estadoAceptacion = estadosAceptacion[index];
+        if (palabraTamanio === 0 && estadoActual === estadoAceptacion) {
+            return
+        }
+    }
+    const nodosAdyacentes = funcionTransicion[estadoActual];
+    console.log("nodosAdyacentes " + nodosAdyacentes)
+    let contador = 0;
+    for (const nodoAdyacente of nodosAdyacentes) {
+        contador += 1;
+        if (Object.keys(nodosAdyacentes).length === contador) {
+            console.log("Keys " + nodosAdyacentes[1]);
+            console.log("nodoAdyacente[1]  " + nodoAdyacente[1])
+            let position = nodoAdyacente.indexOf(estadosAceptacion[0])
+            if (position !== -1) {
+                estadoActual = nodoAdyacente[position];
+                mostrarImagen()
+            }
+        }
+        if (nodoAdyacente.includes(palabra[0])) {
+            console.log(nodoAdyacente);
+            if (nodoAdyacente[1].length === 0) {
+                console.log(nodoAdyacente[1]);
+                continue;
+            }
+            console.log("\n")
+            return cambiarEstado(palabra.slice(1, palabraTamanio), nodoAdyacente[1]);
+        }
+    }
 }
 
-if (chequearPalabraIngresada(palabraUsuario)) {
-    cambiarEstado(palabraUsuario) 
-    if (estadoActual == estadoAceptacion) {
-        console.log("Palabra Aceptada")
-    } else {
-        console.log("Palabra No Aceptada")
-    }
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-
